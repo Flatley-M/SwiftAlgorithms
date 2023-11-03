39948613b77171c9172ebb1c5498c7e8bac26d38
Michael Flatley <flatley.m@etoncollege.org.uk>
2023-11-03 17:12:38 +0000

    insertion/ bubble sort thingy

--- SwiftAlgorithms/main.swift
+++ SwiftAlgorithms/main.swift
@@ -7,5 +7,9 @@
 
 import Foundation
 
-let array = [1,2,3,4,67,6,33,44,8,12,54,76,36,24,98,53,42,23,43,52,7]
+var array = [1,2,3,4,67,6,33,44,8,12,54,76,36,24,98,53,42,23,43,52,7]
 var num = 52
+
+insertionSort(data: &array)
+
+//linearSearch(num: num)
--- SwiftAlgorithms/sorting/Insertion Sort.swift
+++ SwiftAlgorithms/sorting/Insertion Sort.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
-func insertionSort(data: inout (Int)){
-    
+func insertionSort(data: inout [Int]){
+    var holding = 0
+    for i in 0..<data.count {
+        //print(i)
+        if i > 0 && data[i] < data[i-1]{
+            holding = data[i-1]
+            data[i-1] = data[i]
+            data[i] = holding
+        }
+    }
+    print(data)
 }
